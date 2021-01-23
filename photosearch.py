054938c659b426531741cfe39b5d2ceb4ed1f7d8
JoeKahora <gachau.joel@gmail.com>
2021-01-23 12:51:37 +0300

    -add a key=value pair to the empty dictionary parameter for the apikey.

--- photosearch.py
+++ photosearch.py
@@ -7,3 +7,4 @@ flickrApiKey = ""
 def flickr_api_response(tagstring):
     baseurl = "https://api.flickr.com/services/rest/"
     dictionary_parameters = {}
+    dictionary_parameters["apikey"] = flickrApiKey
